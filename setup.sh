0a5c60580e5a7190cf783ec9a5bfbea99069ca38
NearHuscarl <near.huscarl@gmail.com>
2018-09-23 17:05:47 +0700

    [fix] various errors on window
    install vim and neovim using chocolatey package manager (window)
    fix wrong warning message when install neovim module
    fix install plugin not working on window
    remove neovim hardcode path on window
    other tweaks and cleanup

--- setup.sh
+++ setup.sh
@@ -1,21 +1,48 @@
 #!/bin/bash
 
+# script to setup vim and neovim config files
+
+# Run this script as admin
+# On window: Hold CTRL+SHIFT while clicking git bash icon
+
 # This script is part of vim-config
 # Run to setup vim-config for vim and neovim on windows
 
 # Requirement:
-# MinGW
+# git bash (Window)
+# chocolatey (Window)
 # pip
 # npm
-
-# Note: if you have git installed. It already included
-# MinGW. Right click and select 'Git bash'
 
 # COLORS {{{
 GREEN="$(tput setaf 2)"
 YELLOW="$(tput setaf 3)"
 RESET="$(tput sgr0)"
 # }}}
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+IS_WINDOW=''
+OS=$(uname -s)
+
+if [[ "$OS" =~ 'CYGWIN' ]] || [[ "$OS" =~ 'MINGW' ]]; then
+	IS_WINDOW=true
+else
+	IS_WINDOW=false
+fi
+
+if [[ "$IS_WINDOW" == true ]]; then
+	VIM_PATH="$HOME/vimfiles"
+else
+	VIM_PATH="$HOME/.vim"
+fi
+
+if [[ "$IS_WINDOW" == true ]]; then
+	NVIM_PATH="$LOCALAPPDATA/nvim"
+else
+	NVIM_PATH="$HOME/.config/nvim"
+fi
+
+
 print_progress() { #{{{
 	# Console width number
 	T_COLS="$(tput cols)"
@@ -24,47 +51,27 @@ print_progress() { #{{{
 # }}}
 print_warning() { #{{{
 	T_COLS="$(tput cols)"
-	echo -e "  ${YELLOW}$1${RESET}\n" | fold -sw $(($T_COLS - 2))
+	echo -e " ${YELLOW}$1${RESET}\n" | fold -sw $(($T_COLS - 2))
+}
+# }}}
+has_command() { # {{{
+	command -v $1 > /dev/null
 }
 # }}}
 
-function get_os() { # {{{
-	os=$(uname -s)
-	if [[ "$os" =~ 'CYGWIN' ]] || [[ "$os" =~ 'MINGW' ]]; then
-		echo 'win'
-	elif [[ "$os" == 'Linux' ]]; then
-		echo 'linux'
-	fi
-}
-# }}}
-function get_vimpath() { # {{{
-	local os
-	os="$(get_os)"
-	if [[ "$os" == 'win' ]]; then
-		echo "$HOME/vimfiles"
-	elif [[ "$os" == 'linux' ]]; then
-		echo "$HOME/.vim"
-	fi
-}
-# }}}
-function get_nvimpath() { # {{{
-	local os
-	os="$(get_os)"
-	if [[ "$os" == 'win' ]]; then
-		echo 'C:/Users/Near/AppData/Local/nvim'
-	elif [[ "$os" == 'linux' ]]; then
-		echo "$HOME/.config/nvim"
-	fi
-}
-# }}}
 
-OS="$(get_os)"
-VIM_PATH="$(get_vimpath)"
-NVIM_PATH="$(get_nvimpath)"
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
-
+install_vim() { # {{{
+	if [[ "$IS_WINDOW" == true ]]; then
+		if has_command choco; then
+			choco install neovim -y
+			choco install vim-tux -y
+		else
+			print_warning 'chocolatey is not installed. Skip installing vim and neovim...'
+		fi
+	fi
+}
+#}}}
 move_repo() { # {{{
-	print_progress "move vim-config to $VIM_PATH"
 	if [[ "$SCRIPT_DIR" != "$VIM_PATH" ]]; then
 		print_progress "copy $SCRIPT_DIR to $VIM_PATH"
 		mv -r "$SCRIPT_DIR" "$VIM_PATH"
@@ -108,21 +115,30 @@ download_neovim_init() { # {{{
 }
 # }}}
 install_neovim_module_for_python() { # {{{
-	# Install neovim for python and javascript if unavailable
+	if ! has_command pip; then
+		print_warning 'pip not installed. Skipping install neovim module for python'
+		return
+	fi
+
 	if ! python -c 'import neovim' 2> /dev/null; then
 		print_progress 'Install neovim for python'
 		pip install --user neovim
 	else
-		print_warning 'pip not installed. Skipping install neovim module for python'
+		print_warning 'neovim module for python is already installed'
 	fi
 }
 # }}}
 install_neovim_module_for_javascript() { # {{{
+	if ! has_command npm; then
+		print_warning 'npm not installed. Skipping install neovim module for javascript'
+		return
+	fi
+
 	if ! npm list --depth 0 --global neovim &> /dev/null; then
 		print_progress 'Install neovim for javascript'
 		npm install --global neovim
 	else
-		print_warning 'npm not installed. Skipping install neovim module for javascript'
+		print_warning 'neovim package for javascript is already installed'
 	fi
 }
 # }}}
@@ -134,10 +150,16 @@ setup_neovim() { # {{{
 # }}}
 download_plugins() { # {{{
 	print_progress "Install plugins..."
-	nvim +PlugInstall +qa
+
+	if [[ "$IS_WINDOW" == true ]]; then
+		nvim-qt.exe +PlugInstall +qa
+	else
+		vim +PlugInstall +qa
+	fi
 } # }}}
 setup_vim() { # {{{
 	move_repo
+	install_vim
 	create_required_dirs
 	download_plug
 	setup_fzf
